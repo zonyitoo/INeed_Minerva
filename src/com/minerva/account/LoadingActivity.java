9a384b8d603c7f72dd8cb2bffdb0e5c30bb7d6e7
Zonyitoo <zonyitoo@gmail.com>
2012-03-24 02:25:05 +0800

    3.24，改变加载界面加载代码，简略写MainActivity界面实现，用户数据库及请求数据库的设计

--- src/com/minerva/account/LoadingActivity.java
+++ src/com/minerva/account/LoadingActivity.java
@@ -2,51 +2,61 @@ package com.minerva.account;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.view.WindowManager;
 
 import com.minerva.R;
+import com.minerva.core.MainActivity;
+import com.minerva.utils.UtilConstant;
 
-public class LoadingActivity extends Activity implements OnClickListener {
-    Button reg;
-    LinearLayout logon_layout;
-    LinearLayout top;
-    Animation logon_anim;
+public class LoadingActivity extends Activity{
 	
+	SharedPreferences userPrefs;
+	String user_name;
+	long user_id;
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
         
-        logon_anim = AnimationUtils.loadAnimation(this, R.anim.anim_splash_screen);
+        userPrefs = getSharedPreferences(UtilConstant.LOGGED_USER_PREFS, MODE_PRIVATE);
         
-        reg = (Button) findViewById(R.id.button_reg);
-        reg.setOnClickListener(this);
-        logon_layout = (LinearLayout) findViewById(R.id.linearlayout_splashscreen_logon);
-        top = (LinearLayout) findViewById(R.id.linearlayout_splashscreen_top);
-        top.setVisibility(LinearLayout.VISIBLE);
-        
-        logon_layout.setVisibility(LinearLayout.VISIBLE);
-		logon_layout.startAnimation(logon_anim);
-		reg.setVisibility(Button.VISIBLE);
+        new LoadingThread().start();
     }
 
 	@Override
-	public void onClick(View v) {
-		startActivityForResult(new Intent(this, RegActivity.class), 1);
+	protected void onStop() {
+		super.onStop();
+		
+		//startActivity(new Intent(this, MainActivity.class));
 	}
+    
+    class LoadingThread extends Thread {
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// TODO Auto-generated method stub
-		super.onActivityResult(requestCode, resultCode, data);
-	}
-	
-	
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+	        user_id = userPrefs.getLong(UtilConstant.PREFS_USER_ID, -1);
+	        if (user_id == -1) {
+	        	startActivity(new Intent(LoadingActivity.this, RegActivity.class));
+	        }
+	        else {
+	        	user_name = userPrefs.getString(UtilConstant.PREFS_USER_NAME, "");
+	        	startActivity(new Intent(LoadingActivity.this, MainActivity.class)
+	        		.putExtra(UtilConstant.PREFS_USER_NAME, user_name)
+	        		.putExtra(UtilConstant.PREFS_USER_ID, user_id));
+	        }
+	        
+	        LoadingActivity.this.finish();
+		}
+    	
+    }
 }
--- src/com/minerva/account/RegActivity.java
+++ src/com/minerva/account/RegActivity.java
@@ -1,17 +1,63 @@
 package com.minerva.account;
 
-import com.minerva.R;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class RegActivity extends Activity {
+import com.minerva.R;
+import com.minerva.core.INeedApplication;
+import com.minerva.core.MainActivity;
+import com.minerva.utils.UtilConstant;
 
+public class RegActivity extends Activity implements OnClickListener {
+	
+	EditText userName;
+	EditText password;
+	EditText password2;
+	ImageView userLogo;
+	Button reg;
+	
+	INeedApplication app;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
+		
+		app = (INeedApplication) getApplication();
+		
+		userName = (EditText) findViewById(R.id.edittext_regactivity_username);
+		password = (EditText) findViewById(R.id.edittext_regactivity_passwd);
+		password2 = (EditText) findViewById(R.id.edittext_regactivity_passwd2);
+		userLogo = (ImageView) findViewById(R.id.imageview_regactivity_userlogo);
+		reg = (Button) findViewById(R.id.button_regactivity_reg);
+		
+		reg.setOnClickListener(this);
+		//userName.addTextChangedListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		long user_id = app.addNewUser(userName.getText().toString(), 0, "", "", "", "", 1);
+		
+		startActivity(new Intent(this, MainActivity.class)
+			.putExtra(UtilConstant.PREFS_USER_NAME, userName.getText().toString())
+			.putExtra(UtilConstant.PREFS_USER_ID, user_id));
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) 
+			finish();
+		
+		return super.onKeyDown(keyCode, event);
 	}
 
 }
