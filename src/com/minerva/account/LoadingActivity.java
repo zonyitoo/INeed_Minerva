7d4eaaa6fda4cb3915d259723f8e795bdc994591
Zonyitoo <zonyitoo@gmail.com>
2012-03-25 23:29:03 +0800

    实现开启时登录及注册，不可注册相同用户名..

--- src/com/minerva/account/LoadingActivity.java
+++ src/com/minerva/account/LoadingActivity.java
@@ -1,10 +1,17 @@
 package com.minerva.account;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.WindowManager;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.minerva.R;
 import com.minerva.core.MainActivity;
@@ -15,6 +22,8 @@ public class LoadingActivity extends Activity{
 	SharedPreferences userPrefs;
 	String user_name;
 	long user_id;
+	String user_pwd;
+	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,26 +44,57 @@ public class LoadingActivity extends Activity{
     
     class LoadingThread extends Thread {
 
+    	URL loginURL;
+    	
 		@Override
 		public void run() {
-			try {
-				Thread.sleep(3000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
 	        user_id = userPrefs.getLong(UtilConstant.PREFS_USER_ID, -1);
 	        if (user_id == -1) {
 	        	startActivity(new Intent(LoadingActivity.this, RegActivity.class));
 	        }
 	        else {
 	        	user_name = userPrefs.getString(UtilConstant.PREFS_USER_NAME, "");
-	        	startActivity(new Intent(LoadingActivity.this, MainActivity.class)
-	        		.putExtra(UtilConstant.PREFS_USER_NAME, user_name)
-	        		.putExtra(UtilConstant.PREFS_USER_ID, user_id));
+	        	user_pwd = userPrefs.getString(UtilConstant.PREFS_USER_PWD, "");
+	        	
+	        	// Login server
+	        	
+	        	PrintWriter writer = null;
+	        	int respCode = 0;
+	        	try {
+	     			loginURL = new URL(UtilConstant.SERVER_URL + "/login/");
+	     			HttpURLConnection connection = (HttpURLConnection) loginURL.openConnection();
+	     			
+	     			connection.setRequestMethod("POST");
+	     			connection.setDoInput(true);
+	     			connection.setDoOutput(true);
+	     			
+	     			writer = new PrintWriter(connection.getOutputStream());
+	     			
+	     			writer.print(user_name + "&" + user_pwd);
+	     			writer.flush();
+	     			writer.close();
+	     			
+	     			respCode = connection.getResponseCode();
+	     			Log.d(UtilConstant.DEBUG_TAG, "respCode=" + respCode);
+	        	} catch (MalformedURLException e) {
+	     			// TODO Auto-generated catch block
+	     			e.printStackTrace();
+	     		} catch (IOException e) {
+	     			// TODO Auto-generated catch block
+	     			e.printStackTrace();
+	     		}
+	        	
+	        	if (respCode == UtilConstant.RESPONSE_SUCCEED_CODE) {
+	        		startActivity(new Intent(LoadingActivity.this, MainActivity.class)
+	        			.putExtra(UtilConstant.PREFS_USER_NAME, user_name)
+	        			.putExtra(UtilConstant.PREFS_USER_ID, user_id));
+	        	}
+	        	else {
+	        		Toast.makeText(LoadingActivity.this, R.string.loading_login_err, Toast.LENGTH_SHORT).show();
+	        		startActivity(new Intent(LoadingActivity.this, RegActivity.class));
+	        	}
+	        	
 	        }
-	        
 	        LoadingActivity.this.finish();
 		}
     	
--- src/com/minerva/account/RegActivity.java
+++ src/com/minerva/account/RegActivity.java
@@ -1,14 +1,26 @@
 package com.minerva.account;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.minerva.R;
 import com.minerva.core.INeedApplication;
@@ -24,6 +36,8 @@ public class RegActivity extends Activity implements OnClickListener {
 	Button reg;
 	
 	INeedApplication app;
+	
+	ProgressDialog loadingDialog;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,16 +55,88 @@ public class RegActivity extends Activity implements OnClickListener {
 		
 		reg.setOnClickListener(this);
 		//userName.addTextChangedListener(this);
+		
+		loadingDialog = new ProgressDialog(this);
+		loadingDialog.setMessage("Loading");
+		loadingDialog.setCancelable(false);
 	}
 
 	@Override
 	public void onClick(View v) {
-		long user_id = app.addNewUser(userName.getText().toString(), 0, "", "", "", "", 1);
 		
-		startActivity(new Intent(this, MainActivity.class)
-			.putExtra(UtilConstant.PREFS_USER_NAME, userName.getText().toString())
-			.putExtra(UtilConstant.PREFS_USER_ID, user_id));
-		finish();
+		if (!password.getText().toString().equals(password2.getText().toString())) {
+			Toast.makeText(this, "password must be the same!", Toast.LENGTH_SHORT).show();
+			return;
+		}
+		
+		loadingDialog.show();
+		new CommunicateWithServer().execute(null);
+		/*
+		final ProgressDialog progress = new ProgressDialog(this);
+		progress.setMessage("Loading");
+		progress.setCancelable(false);
+		progress.show();
+		
+		new Thread() {
+
+			@Override
+			public void run() {
+				// register
+				try {
+					URL registerURL = new URL(UtilConstant.SERVER_URL + "/register/");
+					HttpURLConnection connection = (HttpURLConnection) registerURL.openConnection();
+							
+					connection.setRequestMethod("POST");
+					connection.setInstanceFollowRedirects(true);
+					connection.setDoInput(true);
+					connection.setDoOutput(true);
+							
+					connection.connect();
+							
+					PrintWriter writer = new PrintWriter(connection.getOutputStream());
+					writer.print(userName.getText().toString().trim() + "&" + password.getText().toString() + "&" + userName.getText().toString().trim());
+					writer.flush();
+					writer.close();
+							
+					//System.out.println(connection.getResponseMessage());
+							
+					if (connection.getResponseCode() == 200) {
+						InputStreamReader isr = new InputStreamReader(connection.getInputStream());
+						BufferedReader reader = new BufferedReader(isr);
+								
+						String line = null;
+								
+						while ((line = reader.readLine()) != null) {
+							//System.out.println(line);
+							Log.d(UtilConstant.DEBUG_TAG, line);
+						}
+						
+						long user_id = app.addNewUser(userName.getText().toString(), 0, "", "", "", "", 1);
+						
+						startActivity(new Intent(RegActivity.this, MainActivity.class)
+							.putExtra(UtilConstant.PREFS_USER_NAME, userName.getText().toString().trim())
+							.putExtra(UtilConstant.PREFS_USER_ID, user_id));
+						finish();
+					}
+					
+					else {
+						Toast.makeText(RegActivity.this, "ERROR", Toast.LENGTH_LONG).show();
+					}
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				progress.cancel();
+			}
+			
+		};
+		*/
+		
+		
 	}
 
 	@Override
@@ -59,6 +145,69 @@ public class RegActivity extends Activity implements OnClickListener {
 			finish();
 		
 		return super.onKeyDown(keyCode, event);
+	}
+	
+	class CommunicateWithServer extends AsyncTask<String, Integer, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+			try {
+				URL registerURL = new URL(UtilConstant.SERVER_URL + "/register/");
+				HttpURLConnection connection = (HttpURLConnection) registerURL.openConnection();
+						
+				connection.setRequestMethod("POST");
+				connection.setInstanceFollowRedirects(true);
+				connection.setDoInput(true);
+				connection.setDoOutput(true);
+						
+				connection.connect();
+						
+				PrintWriter writer = new PrintWriter(connection.getOutputStream());
+				writer.print(userName.getText().toString().trim() + "&" + password.getText().toString() + "&" + userName.getText().toString().trim());
+				writer.flush();
+				writer.close();
+						
+				//System.out.println(connection.getResponseMessage());
+						
+				if (connection.getResponseCode() == 200) {
+					InputStreamReader isr = new InputStreamReader(connection.getInputStream());
+					BufferedReader reader = new BufferedReader(isr);
+							
+					String line = null;
+							
+					while ((line = reader.readLine()) != null) {
+						//System.out.println(line);
+						Log.d(UtilConstant.DEBUG_TAG, line);
+					}
+					
+					long user_id = app.addNewUser(userName.getText().toString(), 0, "", "", "", "", 1);
+					
+					startActivity(new Intent(RegActivity.this, MainActivity.class)
+						.putExtra(UtilConstant.PREFS_USER_NAME, userName.getText().toString().trim())
+						.putExtra(UtilConstant.PREFS_USER_ID, user_id));
+					finish();
+				}
+				
+				else {
+					Toast.makeText(RegActivity.this, "ERROR", Toast.LENGTH_LONG).show();
+				}
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			loadingDialog.dismiss();
+		}
+		
+		
 	}
 
 }
